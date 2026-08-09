7213487deb1f1bc73010f701e532c8e01f9610b8
An <An3439201@gmail.com>
2026-08-09 17:23:27 +0700

    feat: implement IUserTokenService, add OTP verification strategy resolver, and update VerifyOtp command workflow

--- Services/IdentityService/UavPms.IdentityService.Application/Features/Auth/Commands/VerifyOtp/Strategies/ForgotPasswordOtpStrategy.cs
+++ Services/IdentityService/UavPms.IdentityService.Application/Features/Auth/Commands/VerifyOtp/Strategies/ForgotPasswordOtpStrategy.cs
@@ -1,6 +1,36 @@
+using System.Security.Claims;
+using UavPms.IdentityService.Application.Common.Utilities;
+using UavPms.IdentityService.Application.Features.Auth.DTOs;
+using UavPms.IdentityService.Domain.Entities;
+using UavPms.IdentityService.Domain.Enums;
+using UavPms.IdentityService.Domain.Interfaces.Services;
+
 namespace UavPms.IdentityService.Application.Features.Auth.Commands.VerifyOtp.Strategies;
 
-public class ForgotPasswordStrategy
+public class ForgotPasswordOtpStrategy : IOtpVerificationStrategy
 {
-    
+    private readonly IOtpService _otpService;
+
+    public ForgotPasswordOtpStrategy(IOtpService otpService)
+    {
+        _otpService = otpService;
+    }
+
+    public bool CanHandle(OtpPurpose purpose)
+        => purpose == OtpPurpose.ForgotPassword;
+
+    public async Task<OtpVerifyResultDto> VerifyAsync(User user, VerifyOtpCommand request, CancellationToken cancellationToken)
+    {
+        var token = Guid.NewGuid().ToString();
+        var hash = TokenHasher.Hash(token);
+        
+        await _otpService.SaveVerificationTokenAsync(hash, user.Email, TimeSpan.FromMinutes(10));
+
+        return new OtpVerifyResultDto
+        {
+            Success = true,
+            Message = "Verification Successful",
+            Token = token
+        };
+    }
 }
