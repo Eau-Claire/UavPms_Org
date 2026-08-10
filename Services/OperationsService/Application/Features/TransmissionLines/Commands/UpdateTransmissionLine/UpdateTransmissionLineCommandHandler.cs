01c1f8d08c845d936000b0f082436147109f4ea0
bunpmc <phamhoangminhchau1973@gmail.com>
2026-08-10 11:18:55 +0700

    fix: clean linux build compatibility

--- Services/OperationsService/Application/Features/TransmissionLines/Commands/UpdateTransmissionLine/UpdateTransmissionLineCommandHandler.cs
+++ Services/OperationsService/Application/Features/TransmissionLines/Commands/UpdateTransmissionLine/UpdateTransmissionLineCommandHandler.cs
@@ -1,4 +1,3 @@
-using System.Runtime.InteropServices.JavaScript;
 using MediatR;
 using NetTopologySuite;
 using NetTopologySuite.Geometries;
@@ -42,8 +41,7 @@ public class UpdateTransmissionLineCommandHandler : IRequestHandler<UpdateTransm
         Geometry? geom = null;
         if (!string.IsNullOrEmpty(request.GeomWkt))
         {
-            var geometryFactory = NtsGeometryServices.Instance.CreateGeometryFactory(srid: 4326);
-            var wktReader = new WKTReader(geometryFactory);
+            var wktReader = new WKTReader(new NtsGeometryServices(new PrecisionModel(), 4326));
 
             try
             {
