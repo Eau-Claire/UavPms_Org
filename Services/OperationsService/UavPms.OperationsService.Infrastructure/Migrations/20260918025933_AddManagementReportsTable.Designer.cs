9ddef4926b5a90abbeec8f55f3d372f88d3490ef
an343 <An3439201@gmail.com>
2026-09-18 10:02:42 +0700

    feat(operations): add EF Core migration for management reports tables

--- Services/OperationsService/UavPms.OperationsService.Infrastructure/Migrations/20260918025933_AddManagementReportsTable.Designer.cs
+++ Services/OperationsService/UavPms.OperationsService.Infrastructure/Migrations/20260918025933_AddManagementReportsTable.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetTopologySuite.Geometries;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -12,9 +13,11 @@ using UavPms.OperationsService.Infrastructure.Persistence;
 namespace UavPms.OperationsService.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260918025933_AddManagementReportsTable")]
+    partial class AddManagementReportsTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
